04cbaa141cd75950e11c63fbd72d5c44936c1f04
Sodole <lws0915@gmail.com>
2023-05-28 00:46:57 +0900

    deploy test

---
+++ src/main/java/com/shop/dto/OrderHistDto.java
@@ -0,0 +1,31 @@
+package com.shop.dto;
+
+import com.shop.constant.OrderStatus;
+import com.shop.entity.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderHistDto {
+
+    private final Long orderId;
+    private final String orderDate;
+    private final OrderStatus orderStatus;
+
+    public OrderHistDto(Order order){
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.orderStatus = order.getOrderStatus();
+    }
+
+    private List<OrderItemDto> orderItemDtoList = new ArrayList<>();
+
+    public void addOrderItemDto(OrderItemDto orderItemDto){
+        orderItemDtoList.add(orderItemDto);
+    }
+}
